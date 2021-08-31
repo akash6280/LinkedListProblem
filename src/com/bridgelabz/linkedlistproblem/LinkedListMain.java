71c09b8114152722cf32bb57a827f4885cfd8544
Akash Singh <akashsingh6280@gmail.com>
2021-09-01 01:24:08 +0530

    ADDED: Creating linked list by adding node in beginning

--- src/com/bridgelabz/linkedlistproblem/LinkedListMain.java
+++ src/com/bridgelabz/linkedlistproblem/LinkedListMain.java
@@ -2,11 +2,13 @@ package com.bridgelabz.linkedlistproblem;
 
 public class LinkedListMain {
 	public static void main(String[] args) {
-		Node<Integer> firstNode = new Node(56);
+		Node<Integer> firstNode = new Node(70);
 		Node<Integer> secondNode = new Node(30);
-		Node<Integer> thirdNode = new Node(70);
+		Node<Integer> thirdNode = new Node(56);
+		LinkedList myLinkedList= new LinkedList();
+		myLinkedList.add(firstNode);
+		myLinkedList.add(secondNode);
+		myLinkedList.add(thirdNode);
 		
-		firstNode.setNext(secondNode);
-		secondNode.setNext(thirdNode);
 	}
 }
