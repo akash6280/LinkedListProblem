95884c49d4f6b11609bf8b475ddac1a373f034e4
Akash Singh <akashsingh6280@gmail.com>
2021-09-01 01:37:18 +0530

    ADDED: Calling append method to add node at end of linked list

--- src/com/bridgelabz/linkedlistproblem/LinkedListMain.java
+++ src/com/bridgelabz/linkedlistproblem/LinkedListMain.java
@@ -2,13 +2,14 @@ package com.bridgelabz.linkedlistproblem;
 
 public class LinkedListMain {
 	public static void main(String[] args) {
-		Node<Integer> firstNode = new Node(70);
+		Node<Integer> firstNode = new Node(56);
 		Node<Integer> secondNode = new Node(30);
-		Node<Integer> thirdNode = new Node(56);
+		Node<Integer> thirdNode = new Node(70);
 		LinkedList myLinkedList= new LinkedList();
-		myLinkedList.add(firstNode);
-		myLinkedList.add(secondNode);
-		myLinkedList.add(thirdNode);
+		myLinkedList.append(firstNode);
+		myLinkedList.append(secondNode);
+		myLinkedList.append(thirdNode);
+		myLinkedList.printMyNodes();
 		
 	}
 }
